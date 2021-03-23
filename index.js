43ecd573377b898c4e58eac95c1fcc03d5b4a648
Gaganpreet Singh <gagansingh2822@gmail.com>
2021-03-23 13:38:13 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');     // importing the express module or library
 const app = express();                  // calling express as function it generated new application that represents a running express app.
 
-let data = {'message for you':'hello world frox node and express '};
+let data = {'message for you':'hello world from node and express '};
 
 app.get('/',(req,res) =>{
     res.send(data);
