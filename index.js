ce2b087aaef63fe0b3045a608620056c2a064e51
Gaganpreet Singh <gaganpreet.singh@safehouse.technology>
2021-03-23 12:15:14 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');     // importing the express module or library
+const app = express();                  // calling express as function it generated new application that represents a running express app.
+
+let data = {'message1 for you':'hello world frox node and express '};
+
+app.get('/',(req,res) =>{
+    res.send(data);
+});
+
+const Port = process.env.PORT || 5000 ;
+app.listen(Port);
